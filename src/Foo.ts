029fe325b1578d80da0b731807ab4a245fe16b85
Jie Li <JLi147@slb.com>
2022-10-25 17:10:05 +0800

    feat: add functuin

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,22 @@ export class Foo {
     return 'bar'
   }
 }
+
+export function isCuteNumberGreaterThan10(numbers: number[]): boolean {
+  const evenNumbers = findEvenNumbers(numbers)
+  const maxNumber = findMaxNumber(evenNumbers)
+  const actual = isNumberGreaterThan10(maxNumber)
+  return actual
+}
+
+export function findEvenNumbers(numbers: number[]): number[]{
+  return numbers.filter(number => number%2 === 0)
+}
+
+export function findMaxNumber(numbers: number[]): number{
+  return Math.max(...numbers)
+}
+
+export function isNumberGreaterThan10(number: number): boolean{
+  return number > 10
+}
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,20 @@
+import { execPath } from 'process'
 import {Foo} from '../src/Foo'
+import {isCuteNumberGreaterThan10} from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
+})
+
+it('should return true given numbers: 1, 3, 5, 8, 12', () => {
+  // given
+  const numbers = [1, 3, 5, 8, 12]
+
+  // when
+  const actual: boolean = isCuteNumberGreaterThan10(numbers)
+
+  // then
+  expect(actual).toBeTruthy()
 })
